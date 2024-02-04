02a638ff8aac410fc8ffa1b938c5428903029927
zhedron <105975891+zhedron@users.noreply.github.com>
2024-02-04 17:43:48 +0200

    Delete backend/Java directory

--- backend/Java/src/main/java/zhedron/petproject/java/repository/StoreRepository.java
+++
@@ -1,7 +0,0 @@
-package zhedron.petproject.java.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import zhedron.petproject.java.models.Store;
-
-public interface StoreRepository extends JpaRepository<Store, Integer> {
-}
